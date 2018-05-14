83bc7c3ba33c8c314f846d86c6da0e4855b07e41
Ichiji1111 <37737606+Ichiji1111@users.noreply.github.com>
2018-05-14 18:21:20 -0500

    Update CShader.h

--- Engine/CShader.h
+++ Engine/CShader.h
@@ -1,22 +1,46 @@
-class CShader{
+#ifndef CSHADER_H
+#define CSHADER_H
 
-private:
-unsigned int ID;
-public:
-CShader(const std::string& vertexSourceFileName, const std::string& fragmentFileName);
-void operator()(const char* name, int value);
-void operator()(int name, int value);
-void operator()(const char* name, float value);
-void operator()(int name, float value);
-void operator()... // sobrecargar con todos los tipos de datos
-unsigned int GetID()const;
-void Use();
-~CShader();
+#include <string>
+#include "Maths.h"
 
-static std::string ReadFile(const std::string& fileName);
-
-private:
-unsigned int Compile(const char* shaderSource, ShaderType type);
-void Errors(unsigned int shader, ShaderType type);
-
+enum ShaderType
+{
+	VERTEX = 0x8B31,
+	FRAGMENT = 0x8B30,
+	PROGRAM = 0
 };
+
+class CShader
+{
+private:
+	unsigned int ID;
+
+public:
+	CShader();
+	CShader(const std::string& vertexSourceFileName, const std::string& fragmentFileName);
+
+	void operator()(const char* name, int value);
+	void operator()(int name, int value);
+	void operator()(const char* name, float value);
+	void operator()(int name, float value);
+	void operator()(const char* name, const SVector2& vec2);
+	void operator()(int name, const SVector2& vec2);
+	void operator()(const char* name, const SVector3& vec3);
+	void operator()(int name, const SVector3& vec3);
+	void operator()(const char* name, const SVector4& vec4);
+	void operator()(int name, const SVector4& vec4);
+	void operator()(const char* name, const SMatrix4x4& mat4);
+	void operator()(int name, const SMatrix4x4& mat4);
+
+	unsigned int GetID()const;
+	void Use() const;
+	void Delete() const;
+
+	static std::string ReadFile(const std::string& fileName);
+private:
+	unsigned int Compile(const char* shaderSource, ShaderType type);
+	void Errors(unsigned int shader, ShaderType type);
+};
+
+#endif
