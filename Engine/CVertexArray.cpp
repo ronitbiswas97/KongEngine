487176de56e224f206c681713492b79371433551
CristianSerranoMartinez <cris_zcrups@hotmail.com>
2018-05-17 17:10:39 -0500

    CVertexArray fixed2

--- Engine/CVertexArray.cpp
+++ Engine/CVertexArray.cpp
@@ -7,10 +7,10 @@ CVertexArray::CVertexArray()
 void CVertexArray::AddAttribute(int index, int size, size_t stride, const void* offset)
 {
 	glVertexAttribPointer(index, size, GL_FLOAT, GL_FALSE, stride, offset);
+	glEnableVertexAttribArray(0);
 }
 void CVertexArray::Bind()
-{
-	
+{	
 	glBindVertexArray(m_vertexArray);
 }
 void CVertexArray::Unbind() 
