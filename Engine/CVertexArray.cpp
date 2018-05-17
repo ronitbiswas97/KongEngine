af6b8cbb96713c7750e343951526d844626c9614
CristianSerranoMartinez <cris_zcrups@hotmail.com>
2018-05-17 17:07:55 -0500

    VertexArray fixed

--- Engine/CVertexArray.cpp
+++ Engine/CVertexArray.cpp
@@ -1,12 +1,16 @@
 #include <glad\glad.h>
 #include "CVertexArray.h"
+CVertexArray::CVertexArray()
+{
+	glGenVertexArrays(1, &m_vertexArray);
+}
 void CVertexArray::AddAttribute(int index, int size, size_t stride, const void* offset)
 {
 	glVertexAttribPointer(index, size, GL_FLOAT, GL_FALSE, stride, offset);
 }
 void CVertexArray::Bind()
 {
-	glGenVertexArrays(1, &m_vertexArray);
+	
 	glBindVertexArray(m_vertexArray);
 }
 void CVertexArray::Unbind() 
